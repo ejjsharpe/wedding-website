c65709abbc7e95c416e08f774263b1f04b66f103
Elliot Sharpe <ejjsharpe@gmail.com>
2025-11-02 14:14:35 +0000

    feat: add astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   output: "server",
+  adapter: cloudflare(),
+  integrations: [react()],
 });
